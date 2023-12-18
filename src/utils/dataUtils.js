bf2c240a33237f66ccd26576128254b8869ec5a7
ivanavelikova <iveveto@gmail.com>
2023-12-18 14:58:25 +0200

    Add functions to calculate employee pairs for each project

--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -1,3 +1,5 @@
+import { calculateDaysOnProject, calculateOverlappingDays } from "./dateUtils";
+
 function splitString(string) {
   return string.split(/(\r\n|\r|\n)/g);
 }
@@ -18,4 +20,63 @@ function sanitizeArray(array) {
   });
 }
 
-export { splitString, formatArrayToMatrix, sanitizeArray };
+function groupDataByProject(array) {
+  const groupedData = {};
+
+  array.forEach(([employeeID, projectID, start, end]) => {
+    if(!groupedData[projectID]) {
+      groupedData[projectID] = [];
+    }
+
+    groupedData[projectID].push({employeeID, start, end});
+  });
+
+  for (const projectID in groupedData) {
+    if (groupedData.hasOwnProperty(projectID)) {
+      groupedData[projectID].sort((firstEmployee, secondEmployee) => {
+        const firstEmployeeDays = calculateDaysOnProject(firstEmployee.start, firstEmployee.end);
+        const secondEmployeeDays = calculateDaysOnProject(secondEmployee.start, secondEmployee.end);
+
+        return secondEmployeeDays - firstEmployeeDays;
+      });
+    }
+  }
+
+  return groupedData;
+}
+
+function getEmployeePairsForEachProject(groupedData) {
+  const employeePairs = [];
+
+  for (const projectID in groupedData) {
+    if (groupedData.hasOwnProperty(projectID)) {
+      const group = groupedData[projectID];
+      let maxOverlapDays = 0;
+      let maxOverlapPairs = [];
+
+      for (let i = 0; i < group.length - 1; i++) {
+        for (let j = i + 1; j < group.length; j++) {
+          const overlapDays = calculateOverlappingDays(group[i], group[j]);
+
+          if (overlapDays > maxOverlapDays) {
+            maxOverlapDays = overlapDays;
+            maxOverlapPairs = [{ firstEmployee: group[i].employeeID, secondEmployee: group[j].employeeID }];
+          } else if (overlapDays === maxOverlapDays) {
+            maxOverlapPairs.push({ firstEmployee: group[i].employeeID, secondEmployee: group[j].employeeID });
+          }
+        }
+      }
+
+      console.log(`ProjectID: ${projectID}`);
+      console.log("Overlapping days:", maxOverlapDays);
+      console.log("Pairs with the most days working together:", maxOverlapPairs);
+      console.log("--------------------------");
+
+      employeePairs.push({projectID, maxOverlapDays, maxOverlapPairs});
+    }
+  }
+
+  return employeePairs;
+}
+
+export { splitString, formatArrayToMatrix, sanitizeArray, groupDataByProject, getEmployeePairsForEachProject };
