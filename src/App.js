e04cdcd35b7d56954b18a0c0a2fa07cf07841c9e
ivanavelikova <iveveto@gmail.com>
2023-12-21 16:18:39 +0200

    Modify App.js to include file upload component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,22 @@
 import './App.css';
 import { useState } from 'react';
-import { splitString, formatArrayToMatrix, sanitizeArray, groupDataByProject, getEmployeePairsForEachProject } from './utils/dataUtils';
 import DataTable from './components/molecules/DataTable';
+import FileUploader from './components/atoms/FileUploader';
 
 function App() {
   const headingDataTable = "Imported Data";
   const headingsDataTable = ["Employee ID", "Project ID", "Start Date", "End Date"];
   const headingPairsTable = "Longest Employee Pairs on Each Project";
   const headingsPairsTable = ["Project ID", "Employees", "Period", "Days"];
+  const headingFileUpload = "Upload CSV File";
 
   const [data, setData] = useState([]);
   const [result, setResult] = useState([]);
-  const [file, setFile] = useState(null);
-
-  function handleFileChange(e) {
-    setFile(e.target.files[0]);
-  }
-  
-  function handleFileUpload(file) {
-    if (file) {
-      const fileReader = new FileReader();
-  
-      fileReader.readAsText(file);
-  
-      fileReader.onload = function() {
-        const dataArray = splitString(fileReader.result);
-        const dataMatrix = formatArrayToMatrix(dataArray);
-        const sanitizedData = sanitizeArray(dataMatrix);
-  
-        setData(sanitizedData);
-  
-        const groupedEmployeeData = groupDataByProject(sanitizedData);
-        const employeePairs = getEmployeePairsForEachProject(groupedEmployeeData);
-        
-        setResult(employeePairs);
-      }
-    }
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    handleFileUpload(file);
-  }
 
   return (
     <div className="App">
       <div className="container">
-        <h2>Upload CSV file</h2>
-        <form onSubmit={handleSubmit}>
-          <input type={"file"} accept={".csv"} onChange={handleFileChange}/>
-          <button>IMPORT</button>
-        </form>
+        <FileUploader heading={headingFileUpload} setData={setData} setResult={setResult}/>
         <div>
           {data.length > 0 && <DataTable heading={headingDataTable} headings={headingsDataTable} data={data} />}
         </div>
--- src/components/molecules/DataTable.js
+++ src/components/molecules/DataTable.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Heading from "../atoms/Heading";
 import TableHeader from '../atoms/TableHeader';
 import TableBody from '../atoms/TableBody';
 
 function DataTable({heading, headings, data}) {
   return (
     <>
-    <h2>{heading}</h2>
+    <Heading heading={heading}/>
     <table>
       <TableHeader headings={headings}/>
       <TableBody data={data}/>
